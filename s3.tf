df40833e40cfa257d6697a2303dfca52d3382c11
Daniel Hill <danhill@digital.uc.dwp.gov.uk>
2018-08-24 11:51:08 +0100

    Initial notification service to subscribe to SNS topic and send emails

---
+++ s3.tf
@@ -0,0 +1,57 @@
+resource "aws_kms_key" "ses_mailer_bucket_cmk" {
+  description             = "SES Mailer Bucket Master Key"
+  deletion_window_in_days = 14
+  is_enabled              = true
+  enable_key_rotation     = true
+}
+
+resource "aws_s3_bucket" "ses_mailer_bucket" {
+  bucket = "${var.bucket_name}"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  lifecycle_rule {
+    enabled = true
+    noncurrent_version_expiration {
+      days = 30
+    }
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.ses_mailer_bucket_cmk.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+data "aws_iam_policy_document" "ses_send_mail_read_s3" {
+  statement {
+    sid = "SendMailS3BucketReadAccess"
+    effect = "Allow"
+
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.ses_mailer_bucket.arn}",
+      "${aws_s3_bucket.ses_mailer_bucket.arn}/*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "ses_send_mail_read_s3" {
+  name        = "ses_send_mail_read_s3"
+  description = "Allow retreiving mail templates and mailing lists from S3"
+  policy      = "${data.aws_iam_policy_document.ses_send_mail_read_s3.json}"
+}
+
+output "ses_mailer_bucket" {
+  value = "${aws_s3_bucket.ses_mailer_bucket.id}"
+}
---
+++ ses.tf
@@ -0,0 +1,30 @@
+data "aws_iam_policy_document" "ses_send_mail" {
+  statement {
+
+    actions = [
+      "ses:SendRawEmail",
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "ses_send_mail" {
+  name        = "ses_send_mail"
+  description = "Allow sending mail through SES"
+  policy      = "${data.aws_iam_policy_document.ses_send_mail.json}"
+}
+
+resource "aws_ses_domain_identity" "domain_identity" {
+  domain = "${var.domain}"
+}
+
+output "domain_identity" {
+  value = "${aws_ses_domain_identity.domain_identity.domain}"
+}
+
+output "domain_identity_verification_token" {
+  value = "${aws_ses_domain_identity.domain_identity.verification_token}"
+}
