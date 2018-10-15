ed15b6467cae762fcebd590997e7fe0e5a686da1
Daniel Hill <danhill@digital.uc.dwp.gov.uk>
2018-10-15 08:50:10 +0100

    allow mailer to decrypt files in the mailer S3 bucket

--- s3.tf
+++ s3.tf
@@ -36,6 +36,7 @@ data "aws_iam_policy_document" "ses_send_mail_read_s3" {
       "s3:ListBucket",
       "s3:GetObject",
       "s3:GetObjectVersion",
+      "kms:Decrypt",
     ]
 
     resources = [
