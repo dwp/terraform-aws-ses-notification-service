08b2fcfc4540b40edf29d9af92e2b0fcd29f78aa
Daniel Hill <dan@mamu.co>
2019-11-19 09:42:27 +0000

    change `depends_on` and variable `type`s to conform with Terraform 0.12 (#6)
    Signed-off-by: Daniel Hill <danhill@digital.uc.dwp.gov.uk>

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "domain" {
-  type        = "string"
+  type        = string
   description = "The domain name to add to SES identities for sending emails from"
 }
 
 variable "region" {
-  type        = "string"
+  type        = string
   description = "AWS region used by Lambda to communicate with S3 and SES. This should match the AWS provider region"
 }
 
 variable "lambda_sns_to_ses_mailer_zip" {
-  type        = "map"
+  type        = map(string)
   description = "Local path to SES Mailer release: https://github.com/dwp/aws-sns-to-ses-mailer/releases"
   default = {
     base_path = ".",
@@ -18,31 +18,31 @@ variable "lambda_sns_to_ses_mailer_zip" {
 }
 
 variable "log_level" {
-  type        = "string"
+  type        = string
   description = "Logging level for Lambda"
   default     = "info"
 }
 
 variable "log_retention_days" {
-  type        = "string"
+  type        = string
   description = "Number of days to hold logs for"
   default     = "180"
 }
 
 variable "max_threads" {
-  type        = "string"
+  type        = string
   description = "The number of parrallel threads the Lambda can use to send messages to SES. A single thread should handle few hundred thousand."
   default     = "1"
 }
 
 variable "bucket_name" {
-  type        = "string"
+  type        = string
   description = "Name of S3 bucket for storing mailing lists and email templates."
   default     = "ses_notification_service"
 }
 
 variable "bucket_access_logging" {
-  type        = "list"
+  type        = list(map(string))
   description = "To enable access loging on the mailing list and email templates bucket pass in a list of a map of target_bucket and target_prefix"
   default     = []
 }
